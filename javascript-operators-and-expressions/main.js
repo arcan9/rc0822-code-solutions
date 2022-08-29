365416414dc77223092d80ce4f2393226e6f252b
Ashelle Cancio <vermillion707@gmail.com>
2022-08-29 17:53:21 +0000

    Completed javascript-operators-and expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,45 @@
+var width = 250;
+var height = 100;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 100;
+var payment = 150;
+var change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 70;
+var midterm = 85;
+var final = 80;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Ashelle';
+var lastName = 'Cancio';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 12;
+var isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 300;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
