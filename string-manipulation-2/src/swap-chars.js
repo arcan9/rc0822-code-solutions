90a9effd99b07cd3d46be496844fb89e1cf15a83
Ashelle Cancio <vermillion707@gmail.com>
2022-09-05 03:33:22 +0000

    Completed string-manipulation-2 algos.

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,7 +1,13 @@
 /* exported swapChars */
 
 /*
-- Take firstIndex
+String with the characters at firstIndex and secondIndex swapped.
+
+- split the string into an array
+- create a temporary variable as buffer for elements at indexes 1 and 2
+  - it is used to switch value of index 1 with value of index 2
+- now that the characters are swapped, loop through array and convert back into a string
+- return value of newString
 */
 
 function swapChars(firstIndex, secondIndex, string) {
