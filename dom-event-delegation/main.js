b8f6bd762e6b8d38f7ffb2f547c9f8c6f4a8b0c7
Ashelle Cancio <vermillion707@gmail.com>
2022-09-08 19:25:11 +0000

    Modify if statement.

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -5,12 +5,14 @@ $taskList.addEventListener('click', handleClick);
 function handleClick(event) {
   console.log('event.target:', event.target);
   console.log('event.target.tagName:', event.target.tagName);
+
+  if (event.target.tagName !== 'BUTTON') {
+
+    return;
+  }
+
   var closestTaskList = event.target.closest('.task-list-item');
   console.log('closest .task-list-item', closestTaskList);
   closestTaskList.remove();
 
-  if (event.target.tagName !== 'BUTTON') {
-    // eslint-disable-next-line no-useless-return
-    return;
-  }
 }
