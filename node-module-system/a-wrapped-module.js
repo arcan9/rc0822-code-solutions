b49d6f5f2067d637d329121f741576dbc9d94b60
Ashelle Cancio <vermillion707@gmail.com>
2022-10-12 17:12:35 +0000

    Completed node-module-system exercise.

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('type of exports:', typeof exports);
+console.log('value of exports:', exports);
+
+console.log('type of require:', typeof require);
+console.log('value of require:', require);
+
+console.log('type of module:', typeof module);
+console.log('value of module:', module);
+
+console.log('type of __filename:', typeof __filename);
+console.log('value of __filename:', __filename);
+
+console.log('type of __dirname:', typeof __dirname);
+console.log('value of __dirname:', __dirname);
