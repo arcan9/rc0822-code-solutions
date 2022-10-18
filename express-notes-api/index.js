be8d583fff4f13ed18623c689726465b33e356d7
Ashelle Cancio <vermillion707@gmail.com>
2022-10-18 23:40:29 +0000

    Added functionality to check if content prop exists in req.body

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,33 +1,34 @@
 const express = require('express');
+const dataJSON = require('./data.json');
 const app = express();
 
 app.use(express.json());
 
-// const notesId = 1;
-// const notes = {};
+let notesId = 1;
+const notes = {};
 
 // DUMMY OBJECT
-const notes =
-{
-  1: {
-    content: 'This is some content',
-    id: 1
-  },
-  2: {
-    content: 'This is some MORE content',
-    id: 2
-  },
-  3: {
-    content: 'Bla bla bla bla bla',
-    id: 3
-  }
-};
+// const notes =
+// {
+//   1: {
+//     content: 'This is some content',
+//     id: 1
+//   },
+//   2: {
+//     content: 'This is some MORE content',
+//     id: 2
+//   },
+//   3: {
+//     content: 'Bla bla bla bla bla',
+//     id: 3
+//   }
+// };
 
 app.get('/api/notes', (req, res) => {
   const arr = [];
-  if (Object.keys(notes).length > 0) {
-    for (const id in notes) {
-      arr.push(notes[id]);
+  if (Object.keys(dataJSON).length >= 0) {
+    for (const id in dataJSON) {
+      arr.push(dataJSON[id]);
     }
   }
   res.json(arr);
@@ -41,6 +42,22 @@ app.get('/api/notes/:id', (req, res) => {
     res.status(404).json({ error: `No note matches the id ${id}` });
   } else {
     res.status(200).json(notes[id]);
+  }
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNotesObj = req.body;
+  const content = req.body.content;
+
+  if (!content) {
+    // eslint-disable-next-line no-console
+    console.log('No content property input');
+    res.status(400).json({ error: 'content is a required field' });
+  } else {
+    const id = notesId++;
+    newNotesObj.id = id;
+    notes[id] = newNotesObj;
+    res.status(201).json(newNotesObj);
   }
 });
 
