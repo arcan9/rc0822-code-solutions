521caef0da2e530ee8e684e85f150744df95a1a9
Ashelle Cancio <vermillion707@gmail.com>
2022-10-18 02:25:52 +0000

    Modified chunk.js

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,11 +1,5 @@
 /* exported chunk */
 
-// take out n elements and store them inside a variable. Slice.
-// there will be n sets of arrays, the one(s) sliced out
-// and the remaining one (if any).
-// transfer those arrays into a new empty array. Push method.
-// return the value of the new array.
-
 function chunk(array, size) {
   var newArr = [];
   var i = 0;
