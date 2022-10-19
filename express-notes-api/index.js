2341c450772c6f4465c9ba35b6b7cd751a51718c
Ashelle Cancio <vermillion707@gmail.com>
2022-10-19 18:07:30 +0000

    Add functionality to (1) add notes to JSON file, (2) delete note from terminal.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const dataJSON = require('./data.json');
+const fs = require('fs');
+
 const dataNotes = dataJSON.notes;
 const app = express();
-
 app.use(express.json());
 
 let dataNextId = dataJSON.nextId;
 
+// get entire list of notes
 app.get('/api/notes', (req, res) => {
   const arr = [];
   if (Object.keys(dataNotes).length >= 0) {
@@ -17,6 +19,7 @@ app.get('/api/notes', (req, res) => {
   res.json(arr);
 });
 
+// gets a specific note that matches an id
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id;
   if (parseInt(id) < 0) {
@@ -28,6 +31,7 @@ app.get('/api/notes/:id', (req, res) => {
   }
 });
 
+// adds a note in data.json
 app.post('/api/notes', (req, res) => {
   const newNotesObj = req.body;
   const content = req.body.content;
@@ -35,19 +39,38 @@ app.post('/api/notes', (req, res) => {
   if (!content) {
     res.status(400).json({ error: 'content is a required field' });
   } else {
-    // create a conditional here that checks if you're adding
-    // a new note between ones that already exist
-    // or if you're adding a new note at the end
-    let id = req.body.id;
-    if (parseInt(id) < Object.keys(dataNotes).length) {
-      newNotesObj.id = id;
-    } else {
-      id = dataNextId++;
-      newNotesObj.id = id;
-    }
-
+    const id = dataNextId++;
+    newNotesObj.id = id;
     dataNotes[id] = newNotesObj;
-    res.status(201).json(newNotesObj);
+
+    fs.writeFile('data.json', JSON.stringify(dataJSON, null, 2), err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'unexpected error occured' });
+      } else {
+        res.status(201).json(newNotesObj);
+      }
+    });
+  }
+});
+
+// deletes a note by id in data.json
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  if (parseInt(id) < 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (!dataNotes[id]) {
+    res.status(404).json({ error: `No note matches the id ${id}` });
+  } else {
+    fs.writeFile('data.json', JSON.stringify(dataJSON, null, 2), err => {
+      delete dataNotes[id];
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'unexpected error occured' });
+      } else {
+        res.sendStatus(204);
+      }
+    });
   }
 });
 
