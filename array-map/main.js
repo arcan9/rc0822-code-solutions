101761f04051e69992ee14077a0fd1bb752748cb
Ashelle Cancio <vermillion707@gmail.com>
2022-10-24 17:41:24 +0000

    Completed array-map exercise.

---
+++ array-map/main.js
@@ -0,0 +1,24 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const languages = [
+  'Hypertext Markup Language',
+  'Cascading Style Sheets',
+  'ECMAScript',
+  'PHP Hypertext Preprocessor',
+  'Structured Query Language'
+];
+
+const doubled = numbers.map(number => number * 2);
+console.log(doubled);
+
+const prices = numbers.map(price => {
+  const fixedPoint = price.toFixed(2);
+  return `$${fixedPoint}`;
+});
+console.log(prices);
+
+const upperCased = languages.map(language => language.toUpperCase());
+console.log(upperCased);
+
+const firstLetters = languages.map(language => language.charAt(0));
+console.log(firstLetters);
