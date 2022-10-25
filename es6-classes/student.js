2ba3a83557a6afd12046168d42154c61c0cfdfac
Ashelle Cancio <vermillion707@gmail.com>
2022-10-25 04:55:58 +0000

    Completed es6-classes exercise.

---
+++ es6-classes/student.js
@@ -0,0 +1,26 @@
+/* exported Student */
+
+// function Student(firstName, lastName, subject) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.subject = subject;
+// }
+
+class Student {
+  constructor(firstName, lastName, subject) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.subject = subject;
+  }
+}
+
+Student.prototype.getFullName = function () {
+  const { firstName, lastName } = this;
+  return `${firstName} ${lastName}`;
+};
+
+Student.prototype.getIntroduction = function () {
+  const { subject } = this;
+  const fullName = this.getFullName();
+  return `Hello, my name is ${fullName} and I am studying ${subject}.`;
+};
