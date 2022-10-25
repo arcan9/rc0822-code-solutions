eba4238a3bac0dee6fea1567e5256594a72eb2ac
Ashelle Cancio <vermillion707@gmail.com>
2022-10-25 15:49:05 +0000

    Modified student.js

--- es6-classes/student.js
+++ es6-classes/student.js
@@ -6,21 +6,32 @@
 //   this.subject = subject;
 // }
 
+// Student.prototype.getFullName = function () {
+//   const { firstName, lastName } = this;
+//   return `${firstName} ${lastName}`;
+// };
+
+// Student.prototype.getIntroduction = function () {
+//   const { subject } = this;
+//   const fullName = this.getFullName();
+//   return `Hello, my name is ${fullName} and I am studying ${subject}.`;
+// };
+
 class Student {
   constructor(firstName, lastName, subject) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.subject = subject;
   }
+
+  getFullName() {
+    const { firstName, lastName } = this;
+    return `${firstName} ${lastName}`;
+  }
+
+  getIntroduction() {
+    const { subject } = this;
+    const fullName = this.getFullName();
+    return `Hello, my name is ${fullName} and I am studying ${subject}.`;
+  }
 }
-
-Student.prototype.getFullName = function () {
-  const { firstName, lastName } = this;
-  return `${firstName} ${lastName}`;
-};
-
-Student.prototype.getIntroduction = function () {
-  const { subject } = this;
-  const fullName = this.getFullName();
-  return `Hello, my name is ${fullName} and I am studying ${subject}.`;
-};
