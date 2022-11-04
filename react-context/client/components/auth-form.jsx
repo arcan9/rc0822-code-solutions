3d0dad28a27f3a91813edb386d33e5a7ba41c4c4
Ashelle Cancio <vermillion707@gmail.com>
2022-11-04 22:22:55 +0000

    Deleted contextType from AuthForm component

--- react-context/client/components/auth-form.jsx
+++ react-context/client/components/auth-form.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import AppContext from '../lib/app-context';
 
 export default class AuthForm extends React.Component {
   constructor(props) {
@@ -91,5 +90,3 @@ export default class AuthForm extends React.Component {
     );
   }
 }
-
-AuthForm.contextType = AppContext;
